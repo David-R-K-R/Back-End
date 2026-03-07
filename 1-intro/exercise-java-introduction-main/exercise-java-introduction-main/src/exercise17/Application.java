7ad2476d55bf1c1323a9d03a64ba6632782364f3
David-R-K-R <r0949116@ucll.be>
2026-03-07 17:19:44 +0100

    1.2

--- 1-intro/exercise-java-introduction-main/exercise-java-introduction-main/src/exercise17/Application.java
+++ 1-intro/exercise-java-introduction-main/exercise-java-introduction-main/src/exercise17/Application.java
@@ -2,24 +2,34 @@ package exercise17;
 
 public class Application {
 
+    public static void main(String[] args){
+        greet();
+        System.out.println(isEven(2));
+        System.out.println(isEven(3));
+        System.out.println(welcomeMessage("john"));
+        System.out.println(isPositive(9));
+        System.out.println(isPositive(-9));
+
+    }
+
     // Code Sample 1
-    // public String greet() {
-    //     System.out.println("Hello, welcome to the Java course!");
-    // }
+     public static void greet() {
+         System.out.println("Hello, welcome to the Java course!");
+     }
 
     // Code Sample 2
-    // public bool isEven(int number) {
-    //     return number % 2 == 0;
-    // }
+     public static boolean isEven(int number) {
+         return number % 2 == 0;
+     }
 
     // Code Sample 3
-    // public void welcomeMessage(String name) {
-    //      return "Welcome, " + name;
-    // }
+     public static String welcomeMessage(String name) {
+
+        return "Welcome, " + name;
+     }
 
     // Code Sample 4
-    // public boolean isPositive(int number) {
-    //     if (number > 0) {
-    //         return true;
-    //     }
+     public static boolean isPositive(int number) {
+         return number > 0;
+    }
 }
